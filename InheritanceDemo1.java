25cf2938d7d1d414670d171efae34d905b4b4be3
UshaSudulaguntla-11 <ushasudulaguntla11@gmail.com>
2025-09-02 15:57:57 +0530

    Create InheritanceDemo1.java

---
+++ InheritanceDemo1.java
@@ -0,0 +1,23 @@
+class Animal{
+	void eat(){
+		System.out.println("Eating...");
+	}
+}
+class Dog extends Animal{
+	void bark(){
+		System.out.println("Barking");
+	}
+}
+class Puppy extends Dog{
+	void weep(){
+		System.out.println("Weeping...");
+	}
+}
+public class InheritanceDemo1{
+	public static void main(String[] args){
+		Puppy p=new Puppy();
+		p.bark();
+		p.eat();
+		p.weep();
+	}
+}
